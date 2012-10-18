5dc281c1683e3e7a5ee8394a178350cdcb787746
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-10-18 18:31:25 +0900

    デバッグ実行が済んだらFirbugの中身を元に戻すように

--- launch-firefox-mac.sh
+++ launch-firefox-mac.sh
@@ -2,4 +2,8 @@
 cp -f ja-JP/* firebug/extension/locale/ja-JP
 echo "`pwd`/firebug/extension" > profile/extensions/firebug@software.joehewitt.com
 /Applications/Firefox.app/Contents/MacOS/firefox -profile "`pwd`/profile" -no-remote
+pushd .
+cd firebug
+git checkout -- extension/locale/ja-JP/*
+popd
 
